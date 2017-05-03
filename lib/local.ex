55a765e9157222b2c6f2a95a3d8d3d49348ba1e8
xvw <xavier.vdw@gmail.com>
2017-05-04 00:51:46 +0200

    Initialize first macros

--- lib/local.ex
+++ lib/local.ex
@@ -1,18 +1,22 @@
 defmodule Local do
   @moduledoc """
-  Documentation for Local.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Local.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defmacro override(methods, from: a, with: b) do 
+    quote do 
+      import unquote(a), except: unquote(methods)
+      import unquote(b), only: unquote(methods)
+    end
   end
+
+  defmacro scope(module_expr, do: expr) do 
+    quote do 
+      fn() ->
+        unquote(module_expr)
+        unquote(expr)
+      end.()
+    end
+  end
+  
+
 end
--- test/local_test.exs
+++ test/local_test.exs
@@ -2,7 +2,37 @@ defmodule LocalTest do
   use ExUnit.Case
   doctest Local
 
+
+  defmodule Test do 
+
+    def a + b do
+      [a, b] 
+    end
+
+    def a - b do 
+      [b, a]
+    end
+    
+    
+
+  end
+  
+
   test "the truth" do
+
+
+    import Local
+
+    scope (override [+: 2, -: 2], from: Kernel, with: Test) do 
+      IO.inspect 1+1
+    end
+
+    scope (import System) do 
+      IO.inspect user_home()
+    end
+    
+
+
     assert 1 + 1 == 2
   end
 end
