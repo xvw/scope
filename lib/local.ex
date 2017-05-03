bad1970f6e2dd1a3df2a3aa2599cacbf25d6be79
xvw <xavier.vdw@gmail.com>
2017-05-04 00:35:39 +0200

    big bang

---
+++ lib/local.ex
@@ -0,0 +1,18 @@
+defmodule Local do
+  @moduledoc """
+  Documentation for Local.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Local.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/local_test.exs
@@ -0,0 +1,8 @@
+defmodule LocalTest do
+  use ExUnit.Case
+  doctest Local
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
