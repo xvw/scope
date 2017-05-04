1de59ead1bc46b7f9e4f28e996f7e9d3947f47e9
xvw <xavier.vdw@gmail.com>
2017-05-04 02:10:17 +0200

    Change vsn number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Local.Mixfile do
   def project do
     [
       app: :scope,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
